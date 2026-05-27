42464f1def35d720eda5233213315cad22719c51
Leonardo Cabrera <u202415820@upc.edu.pe>
2026-05-27 00:39:59 -0500

    feat(shared): add Analysis aggregate and configure AppDbContext

---
+++ WebWarriors.Aquanetix.Platform/Shared/Domain/Model/Aggregates/Analysis.cs
@@ -0,0 +1,22 @@
+namespace WebWarriors.Aquanetix.Platform.Shared.Domain.Model.Aggregates;
+
+public class Analysis : IAuditableEntity
+{
+    public int Id { get; private set; }
+    public string SensorSourceId { get; private set; }
+    public string DetectedParameters { get; private set; }
+    public string AnomalyStatus { get; private set; }
+    public int SeverityScore { get; private set; }
+    
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+
+    public Analysis(string sensorSourceId, string detectedParameters, 
+        string anomalyStatus, int severityScore)
+    {
+        SensorSourceId = sensorSourceId;
+        DetectedParameters = detectedParameters;
+        AnomalyStatus = anomalyStatus;
+        SeverityScore = severityScore;
+    }
+}
--- WebWarriors.Aquanetix.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ WebWarriors.Aquanetix.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,6 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebWarriors.Aquanetix.Platform.Shared.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Interceptors;
+
 namespace WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddInterceptors(new AuditableEntityInterceptor());
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<Analysis>().HasKey(a => a.Id);
+        builder.Entity<Analysis>().Property(a => a.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Analysis>().Property(a => a.SensorSourceId).IsRequired();
+        builder.Entity<Analysis>().Property(a => a.DetectedParameters).IsRequired();
+        builder.Entity<Analysis>().Property(a => a.AnomalyStatus).IsRequired();
+        builder.Entity<Analysis>().Property(a => a.SeverityScore).IsRequired();
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
 }
