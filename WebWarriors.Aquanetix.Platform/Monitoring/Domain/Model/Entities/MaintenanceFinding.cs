36fe2b4fefd0bbce16bcc749ba26a8727e868216
NicoCSE <u20241D428@upc.edu.pe>
2026-05-31 18:29:59 -0500

    feat(monitoring): Added MaintenanceFinding entity

--- WebWarriors.Aquanetix.Platform/Monitoring/Domain/Model/Entities/MaintenanceFinding.cs
+++ WebWarriors.Aquanetix.Platform/Monitoring/Domain/Model/Entities/MaintenanceFinding.cs
@@ -2,5 +2,14 @@
 
 public class MaintenanceFinding
 {
-    
+    public Guid Id { get; private set; }
+    public string Description { get; private set; }
+    public bool RequiresFollowUp { get; private set; }
+
+    public MaintenanceFinding(string description, bool requiresFollowUp)
+    {
+        Id = Guid.NewGuid();
+        Description = description;
+        RequiresFollowUp = requiresFollowUp;
+    }
 }
