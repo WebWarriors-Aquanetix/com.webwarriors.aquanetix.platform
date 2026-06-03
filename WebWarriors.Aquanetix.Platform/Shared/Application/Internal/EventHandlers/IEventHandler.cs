2cdc039dea19bee4dd96bbafa6b83bf1ac9ceab8
NicoCSE <u20241D428@upc.edu.pe>
2026-06-02 20:06:02 -0500

    feat(shared): add IEventHandler base interface

---
+++ WebWarriors.Aquanetix.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Cortex.Mediator.Notifications;
+using WebWarriors.Aquanetix.Platform.Shared.Domain.Model.Events;
+
+namespace WebWarriors.Aquanetix.Platform.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+}
