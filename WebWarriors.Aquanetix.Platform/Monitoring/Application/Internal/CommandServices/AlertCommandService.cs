586c169251894d345e31d55af3364817e433c083
NicoCSE <u20241D428@upc.edu.pe>
2026-06-08 00:25:40 -0500

    feat(monitoring): add AlertCommandService.cs and AlertQueryService.cs

---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Application/Internal/CommandServices/AlertCommandService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using WebWarriors.Aquanetix.Platform.Monitoring.Application.CommandServices;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Commands;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Repositories;
+using WebWarriors.Aquanetix.Platform.Shared.Application.Model;
+using WebWarriors.Aquanetix.Platform.Shared.Domain.Repositories;
+using WebWarriors.Aquanetix.Platform.Shared.Resources.Errors;
+
+namespace WebWarriors.Aquanetix.Platform.Monitoring.Application.Internal.CommandServices;
+
+public class AlertCommandService(
+    IAlertRepository alertRepository,
+    IUnitOfWork unitOfWork,
+    IStringLocalizer<ErrorMessages> localizer)
+    : IAlertCommandService
+{
+    private readonly IStringLocalizer<ErrorMessages> _localizer = localizer;
+    
+    public async Task<Result<Alert>> Handle(CreateAlertCommand command, CancellationToken cancellationToken)
+    {
+        var alert = new Alert(command);
+        try
+        {
+            await alertRepository.AddAsync(alert, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return Result<Alert>.Success(alert);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<Alert>.Failure(MonitoringError.OperationCancelled,
+                _localizer[nameof(MonitoringError.OperationCancelled)]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Alert>.Failure(MonitoringError.DatabaseError,
+                _localizer[nameof(MonitoringError.DatabaseError)]);
+        }
+        catch (Exception)
+        {
+            return Result<Alert>.Failure(MonitoringError.InternalServerError,
+                _localizer[nameof(MonitoringError.InternalServerError)]);
+        }
+    }
+
+    public async Task<Result<Alert>> Handle(UpdateAlertCommand command, CancellationToken cancellationToken)
+    {
+        var alert = await alertRepository.FindByIdAsync(command.Id, cancellationToken);
+        if (alert is null)
+            return Result<Alert>.Failure(MonitoringError.AlertNotFound,
+                _localizer[nameof(MonitoringError.AlertNotFound)]);
+
+        try
+        {
+            alertRepository.Update(alert);
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return Result<Alert>.Success(alert);
+        }
+        catch (OperationCanceledException)
+        {
+            return Result<Alert>.Failure(MonitoringError.OperationCancelled,
+                _localizer[nameof(MonitoringError.OperationCancelled)]);
+        }
+        catch (DbUpdateException)
+        {
+            return Result<Alert>.Failure(MonitoringError.DatabaseError,
+                _localizer[nameof(MonitoringError.DatabaseError)]);
+        }
+        catch (Exception)
+        {
+            return Result<Alert>.Failure(MonitoringError.InternalServerError,
+                _localizer[nameof(MonitoringError.InternalServerError)]);
+        }
+    }
+}
---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Application/Internal/QueryServices/AlertQueryService.cs
@@ -0,0 +1,18 @@
+﻿using WebWarriors.Aquanetix.Platform.Monitoring.Application.QueryServices;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Queries;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Repositories;
+
+namespace WebWarriors.Aquanetix.Platform.Monitoring.Application.Internal.QueryServices;
+
+public class AlertQueryService(IAlertRepository alertRepository) : IAlertQueryService
+{
+    public async Task<Alert?> Handle(GetAlertByIdQuery query, CancellationToken cancellationToken)
+        => await alertRepository.FindByIdAsync(query.AlertId, cancellationToken);
+
+    public async Task<IEnumerable<Alert>> Handle(GetAllAlertsQuery query, CancellationToken cancellationToken)
+        => await alertRepository.ListAsync(cancellationToken);
+
+    public async Task<IEnumerable<Alert>> Handle(GetAlertsByDeviceIdQuery query, CancellationToken cancellationToken)
+        => await alertRepository.FindByDeviceIdAsync(query.DeviceId, cancellationToken);
+}
