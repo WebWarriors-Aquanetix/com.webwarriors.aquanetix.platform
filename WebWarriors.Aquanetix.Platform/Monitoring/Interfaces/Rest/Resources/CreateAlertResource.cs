54a9da5d60b5030a01c4c8bbaa28baefe347f3bc
NicoCSE <u20241D428@upc.edu.pe>
2026-06-08 00:38:03 -0500

    feat(monitoring): created interface directory and added AlertResource.cs, CreateAlertResource.cs and UpdateAlertResource.cs

---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Interfaces/Rest/Resources/CreateAlertResource.cs
@@ -0,0 +1,13 @@
+﻿namespace WebWarriors.Aquanetix.Platform.Monitoring.Interfaces.Rest.Resources;
+
+public record CreateAlertResource(
+    int    DeviceId,
+    string DeviceName,
+    string Location,
+    string Type,
+    string Severity,
+    string Message,
+    DateTimeOffset Timestamp,
+    string Status,
+    double Value,
+    double Threshold);
