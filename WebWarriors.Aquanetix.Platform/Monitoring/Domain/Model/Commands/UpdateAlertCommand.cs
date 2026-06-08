a8487a103ac0fa70a33c4f7cb6fc705abc2dd500
NicoCSE <u20241D428@upc.edu.pe>
2026-06-07 23:50:16 -0500

    feat(monitoring): add CreateAlertCommand and UpdateAlertCommand

---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Domain/Model/Commands/UpdateAlertCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Commands;
+
+public record UpdateAlertCommand(
+    int           Id,
+    int           DeviceId,
+    string        DeviceName,
+    string        Location,
+    string        Type,
+    string        Severity,
+    string        Message,
+    DateTimeOffset Timestamp,
+    string        Status,
+    double        Value,
+    double        Threshold);
