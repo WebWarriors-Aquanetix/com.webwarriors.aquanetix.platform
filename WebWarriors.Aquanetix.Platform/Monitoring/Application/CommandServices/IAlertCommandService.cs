388e5cdff5bfc36620701d6e9d3d7c827816f970
NicoCSE <u20241D428@upc.edu.pe>
2026-06-08 00:04:46 -0500

    feat(monitoring): add IAlertCommandService.cs and IAlertQueryService.cs

---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Application/CommandServices/IAlertCommandService.cs
@@ -0,0 +1,12 @@
+﻿using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Commands;
+using WebWarriors.Aquanetix.Platform.Shared.Application.Model;
+
+namespace WebWarriors.Aquanetix.Platform.Monitoring.Application.CommandServices;
+
+
+public interface IAlertCommandService
+{
+    Task<Result<Alert>> Handle(CreateAlertCommand command, CancellationToken cancellationToken);
+    Task<Result<Alert>> Handle(UpdateAlertCommand command, CancellationToken cancellationToken);
+}
---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Application/QueryServices/IAlertQueryService.cs
@@ -0,0 +1,12 @@
+﻿using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Queries;
+
+namespace WebWarriors.Aquanetix.Platform.Monitoring.Application.QueryServices;
+
+/// <summary>Query service interface for Alert operations.</summary>
+public interface IAlertQueryService
+{
+    Task<Alert?> Handle(GetAlertByIdQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<Alert>> Handle(GetAllAlertsQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<Alert>> Handle(GetAlertsByDeviceIdQuery query, CancellationToken cancellationToken);
+}
