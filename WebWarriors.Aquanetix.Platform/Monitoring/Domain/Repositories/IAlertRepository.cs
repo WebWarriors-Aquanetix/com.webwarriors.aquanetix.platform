2f49c48ad314450acffad4ee8bb1e1a473708dd4
NicoCSE <u20241D428@upc.edu.pe>
2026-06-08 00:00:39 -0500

    feat(monitoring): add IAlertRepository.cs

---
+++ WebWarriors.Aquanetix.Platform/Monitoring/Domain/Repositories/IAlertRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
+using WebWarriors.Aquanetix.Platform.Shared.Domain.Repositories;
+
+namespace WebWarriors.Aquanetix.Platform.Monitoring.Domain.Repositories;
+
+public interface IAlertRepository : IBaseRepository<Alert>
+{
+    Task<IEnumerable<Alert>> FindByDeviceIdAsync(int deviceId, CancellationToken cancellationToken);
+    
+    Task<IEnumerable<Alert>> FindActiveByDeviceNameAsync(string deviceName, CancellationToken cancellationToken);
+}
