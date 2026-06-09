39a47c314eea8c9d0243553bc376594cc9f5e795
NicoCSE <u20241D428@upc.edu.pe>
2026-06-08 21:55:11 -0500

    fixed(get-alert-by-id): fixed Program.cs

--- WebWarriors.Aquanetix.Platform/Program.cs
+++ WebWarriors.Aquanetix.Platform/Program.cs
@@ -125,7 +125,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.Migrate();
+    context.Database.EnsureCreated();
 }
 
 
