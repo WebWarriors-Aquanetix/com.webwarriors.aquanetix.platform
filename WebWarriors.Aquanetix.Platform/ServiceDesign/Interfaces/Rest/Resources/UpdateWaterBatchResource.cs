d9256896551664e73bd22494b031d17d3e586478
Leonardo Cabrera <u202415820@upc.edu.pe>
2026-06-14 18:40:55 -0500

    feat(service-design): add UpdateWaterBatchResource and assembler for command transformation

---
+++ WebWarriors.Aquanetix.Platform/ServiceDesign/Interfaces/Rest/Resources/UpdateWaterBatchResource.cs
@@ -0,0 +1,9 @@
+namespace WebWarriors.Aquanetix.Platform.ServiceDesign.Interfaces.Rest.Resources;
+
+public record UpdateWaterBatchResource(
+    string         CertificationCode,
+    int            DestinationSectorId,
+    double         VolumeLiters,
+    DateTimeOffset DeliveryTimestamp,
+    string         Status,
+    string         Source);
